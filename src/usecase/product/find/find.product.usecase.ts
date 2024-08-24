0aa9a47814e60f8ae2adcb12ce28a0f551a3b51d
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-08-24 19:23:09 -0300

    feat: find product usecase

---
+++ src/usecase/product/find/find.product.usecase.ts
@@ -0,0 +1,20 @@
+import ProductRepositoryInterface from '../../../domain/product/repository/product-repository.interface';
+import { FindProductInputDto, FindProductOutputDto } from './find.product.dto';
+
+export default class FindProductUseCase {
+  private productRepository: ProductRepositoryInterface
+
+  constructor(productRepository: ProductRepositoryInterface) {
+    this.productRepository = productRepository
+  }
+
+  async excute(input: FindProductInputDto): Promise<FindProductOutputDto> {
+    const product = await this.productRepository.find(input.id)
+    if (!product) throw new Error("Product not found")
+    return {
+      id: product.id,
+      name: product.name,
+      price: product.price
+    }
+  }
+}
