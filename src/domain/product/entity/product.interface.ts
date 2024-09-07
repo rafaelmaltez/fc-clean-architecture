c1dbbc3b47642680fa6c335bf21a5afcc47c74b0
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-09-07 20:53:11 -0300

    refactor: update product interface

--- src/domain/product/entity/product.interface.ts
+++ src/domain/product/entity/product.interface.ts
@@ -2,4 +2,6 @@ export default interface ProductInterface {
   get id(): string;
   get name(): string;
   get price(): number;
+  changeName(newName: string): void
+  changePrice(newPrice: number): void;
 }
