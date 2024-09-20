d49d514cace591a260a0fbcb2557d5639c929c53
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-09-20 18:47:09 -0300

    add route to create product

---
+++ src/infrastructure/api/routes/product.route.ts
@@ -0,0 +1,20 @@
+import {Router, Request, Response } from "express";
+import CreateProductUseCase from "../../../usecase/product/create/create.product.usecase";
+import ProductRepository from "../../product/repository/sequelize/product.repository";
+
+export const productRoute = Router();
+
+productRoute.post("/", async (req: Request, res: Response) => {
+    const usecase = new CreateProductUseCase(new ProductRepository())
+    try {
+        const productDto = {
+            name: req.body.name,
+            price: req.body.price,
+            type: req.body.type
+        }
+        const output = await usecase.execute(productDto)
+        res.send(output)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
