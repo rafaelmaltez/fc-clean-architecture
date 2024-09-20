48f8465a3441e56e3590cb37e4a8f9ca632ca3cb
Rafael Maltez <rafaelmaltez89@gmail.com>
2024-09-20 18:59:36 -0300

    add integration test to list all products route

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -34,4 +34,32 @@ describe("E2E test for product", () => {
     expect(response.status).toBe(500);
   });
 
+  it("Should list all products", async () => {
+    await request(app)
+      .post("/product")
+      .send({
+        name: "Product A",
+        price: 100,
+        type: 'a'
+      });
+    await request(app)
+      .post("/product")
+      .send({
+        name: "Product B",
+        price: 150,
+        type: 'b'
+      });
+    
+      const response = await request(app).get("/product")
+      expect(response.status).toBe(200)
+      console.log("response",response.body)
+      expect(response.body.products.length).toBe(2)
+      expect(response.body.products[0].id).toBeDefined()
+      expect(response.body.products[0].name).toBe("Product A")
+      expect(response.body.products[0].price).toBe(100)
+      expect(response.body.products[1].id).toBeDefined()
+      expect(response.body.products[1].name).toBe("Product B")
+      expect(response.body.products[1].price).toBe(300)
+  })
+
 });
